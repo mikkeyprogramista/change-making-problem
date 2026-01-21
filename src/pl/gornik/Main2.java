6d116b32cc5d4c35cdf811dd5b8f22448337fb14
mikkeyprogramista <mikkeyprogramista@gmail.com>
2026-01-21 13:16:49 +0100

    Add complete project

--- src/pl/gornik/Main2.java
+++ src/pl/gornik/Main2.java
@@ -2,7 +2,7 @@ package pl.gornik;
 
 import java.util.Scanner;
 
-public class Main {
+public class Main2 {
     private static final Scanner SCANNER = new Scanner(System.in);
     private static final int[] CURRENCY = {
             50_000,
@@ -17,6 +17,8 @@ public class Main {
             5,
             2,
             1 };
+    private static final int[] CURRENCY_IN_REGISTER = { 0, 30, 20, 20, 0, 30, 20, 10, 0, 20, 30, 0 };
+    private static boolean possible = true;
 
     public static void main(String[] args) {
         System.out.println("Change-Making Problem!");
@@ -35,19 +37,23 @@ public class Main {
             String end;
 
             System.out.println("Change: ");
-            for (int i = 0; i < countOfCurrency.length; i++) {
-                count = countOfCurrency[i];
-                if (count == 0) continue;
+            if (possible) {
+                for (int i = 0; i < countOfCurrency.length; i++) {
+                    count = countOfCurrency[i];
+                    if (count == 0) continue;
 
-                if (CURRENCY[i] / 100 >= 1) {
-                    value = CURRENCY[i] / 100;
-                    end = "zł";
-                } else {
-                    value = CURRENCY[i];
-                    end = "gr";
+                    if (CURRENCY[i] / 100 >= 1) {
+                        value = CURRENCY[i] / 100;
+                        end = "zł";
+                    } else {
+                        value = CURRENCY[i];
+                        end = "gr";
+                    }
+
+                    System.out.println(count + " x " + value + end);
                 }
-
-                System.out.println(count + " x " + value + end);
+            } else {
+                System.out.println("Cannot return change!");
             }
         }
     }
@@ -55,13 +61,25 @@ public class Main {
     private static int[] getChange(int change) {
         int[] countOfCurrency = new int[CURRENCY.length];
         int i = 0;
+        int count;
 
         while (change > 0) {
-            countOfCurrency[i] = change / CURRENCY[i];
+            count = change / CURRENCY[i];
+            if (CURRENCY_IN_REGISTER[i] >= count) {
+                countOfCurrency[i] = count;
+            } else {
+                countOfCurrency[i] = CURRENCY_IN_REGISTER[i];
+            }
+
             change -= countOfCurrency[i] * CURRENCY[i];
+            CURRENCY_IN_REGISTER[i] -= count;
             i++;
 
             if (change == 0) break;
+            if (i == 12) {
+                possible = false;
+                break;
+            }
         }
 
         return countOfCurrency;
